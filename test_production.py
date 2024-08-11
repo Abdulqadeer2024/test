29823ba65c5d9795837de1619c13b77324bc145b
Abdulqadeer2024 <s223830288@deakin.edu.au>
2024-08-11 20:41:37 +1000

    Create test_production.py

---
+++ test_production.py
@@ -0,0 +1,22 @@
+import pytest
+import numpy as np
+from katabatic.models.production_model import ProductionModel
+
+@pytest.fixture
+def model():
+    return ProductionModel()
+
+def test_fit_and_generate(model):
+    X = np.array([[1, 2], [3, 4], [5, 6]])
+    y = np.array([1, 2, 3])
+    model.fit(X, y)
+    predictions = model.generate(X)
+    assert len(predictions) == 3
+
+def test_evaluate(model):
+    X = np.array([[1, 2], [3, 4], [5, 6]])
+    y = np.array([1, 2, 3])
+    model.fit(X, y)
+    score = model.evaluate(X, y)
+    assert isinstance(score, float)
+    assert 0 <= score <= 1
